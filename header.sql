bd01481414b8f2f4facdf428276bc2c5c3296aff
annie-leung <44278751+annie-leung@users.noreply.github.com>
2018-10-19 11:07:36 +1000

    Add header.sql

---
+++ header.sql
@@ -0,0 +1,9 @@
+--==========================================================================================
+-- Author:      <Author Name>
+-- Create date: <Creation Date>
+-- Description: <Description of script
+-- Last Modified: dd/MM/yyyy - XX - <Change description>
+--
+-- <Sample code for running script if applicable>
+-- ==========================================================================================
+
